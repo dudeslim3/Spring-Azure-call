482d7855c4464b3493aa4fba1398265c99d99b17
unknown <ganeshkulkarni612@gmail.com>
2023-02-20 11:38:03 +0530

    first commit

---
+++ src/main/java/com/example/demo/Servicee2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Servicee2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Servicee2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/mycontroller.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class mycontroller {
+	
+	
+	@GetMapping("/hello1")
+	public String getapi()
+	{
+		System.out.println("hello1");
+		return "hello1";
+	}
+
+}
